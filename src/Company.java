6b481780170aa17ba153d1cd3618c3adc8d0a889
Rafal Paliwoda <r.paliwoda992@gmail.com>
2018-12-01 15:14:15 +0100

    Dopisalem gettery

--- src/Company.java
+++ src/Company.java
@@ -2,5 +2,10 @@ public class Company {
     public static void main (String[] args){
         System.out.println("O CO CHODZI?!");
 
+
+
+        int a == b;
+
+
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -2,4 +2,17 @@ public class Person {
     String name;
     String surname;
     int age;
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
 }
